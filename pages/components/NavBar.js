c4d27a6e39c13f0f17d8bdb5d6c7fee90c24ed03
heojieun <gjekdmlwl@naver.com>
2022-06-24 17:43:47 +0900

    link에 style 넣기 useRouter를 이용해서 현재 위치와 비교하여 style 적용

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -1,11 +1,26 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+  const router = useRouter();
+
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/sub">Sub</Link>
-      <Link href="/hello">Hello</Link>
+      <Link href="/">
+        <a style={{color: router.pathname === "/" ? "red" : "blue"}}>
+          Home
+        </a>
+      </Link>
+      <Link href="/sub">
+        <a style={{color: router.pathname === "/sub" ? "red" : "blue"}}>
+          Sub
+        </a>
+      </Link>
+      <Link href="/hello">
+        <a style={{color: router.pathname === "/hello" ? "red" : "blue"}}>
+          Hello
+        </a>
+      </Link>
     </nav>
   );
 }
