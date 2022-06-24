0519dac3ee3bfd73cf43747a0c39c601b54e2df7
heojieun <gjekdmlwl@naver.com>
2022-06-24 19:02:58 +0900

    styled jsx app 전체에 적용하고 싶을 때는 _app.js 파일을 만들어 적용한다. props으로 Component, pageProps를 사용한다.

--- pages/hello/index.js
+++ pages/hello/index.js
@@ -1,9 +1,6 @@
-import NavBar from "../components/NavBar";
-
 export default function Hello () {
   return (
     <div>
-      <NavBar />
       <h1>Hello pages</h1>
     </div>
   );
--- pages/sub.js
+++ pages/sub.js
@@ -1,9 +1,6 @@
-import NavBar from "./components/NavBar";
-
 export default function Sub() {
   return (
     <div>
-      <NavBar />
       <h1>sub pages</h1>
     </div>
   );
